65cbe4cbd465269189368b8a5803d0ccf2cbe684
Aman Parganiha <72440381+amanparganiha@users.noreply.github.com>
2026-07-26 19:46:56 +0530

    Time: 0 ms (100%), Space: 10.2 MB (74.56%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int robRange(vector<int> &nums , int left , int right){
+        int prev2 = 0;
+        int prev1 = 0;
+
+        for(int i = left ; i <= right ; i++){
+            int curr = max(prev1 , nums[i] + prev2);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) return nums[0];
+
+        return max(robRange(nums , 0 , n-2) , robRange(nums , 1 , n-1));
+    }
+};
